2004b88b109100ea9a16c51924f6cf6cd4ab9924
Brice Schaffner <67745584+ltshb@users.noreply.github.com>
2022-09-08 04:14:45 +0200

    Added force update and clear milestone features (#23)
    * Added force update and clear milestone features
    
    Now we can configure the action to force the update of the milestones if
    it is already set and also to clear the milestone if no milestone is match
    from the PR base/head branches.
    
    * Fixing CI
    
    Use node js v14 to avoid dependencies issues.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { WebhookPayload } from '@actions/github/lib/interfaces';
 
 import config, { match } from './config';
 
+interface Milestone {
+  number: number;
+  title: string;
+  state: string;
+  description: string;
+  due_on: string;
+}
+
 interface PullRequestWebhookPayload extends WebhookPayload {
   // eslint-disable-next-line camelcase
   pull_request?: {
@@ -13,7 +21,7 @@ interface PullRequestWebhookPayload extends WebhookPayload {
     html_url?: string;
     body?: string;
 
-    milestone?: string;
+    milestone?: Milestone;
     base: {
       ref: string;
     },
@@ -23,70 +31,101 @@ interface PullRequestWebhookPayload extends WebhookPayload {
   },
 }
 
+function getMilestoneNumber(
+  milestones: any,
+  _title: string,
+): number | null {
+  const { number } = milestones.data.find(({ title }) => title === _title) || { number: null };
+  return number;
+}
+
+function error(message: string): void {
+  const silent = core.getBooleanInput('silent');
+  console.log(message);
+  if (!silent) {
+    throw Error(message);
+  }
+}
+
 async function run() {
   const token = core.getInput('repo-token', { required: true });
   const configPath = core.getInput('configuration-path', { required: true });
-  const silent = core.getBooleanInput('silent');
+  const force = core.getBooleanInput('force');
+  const clear = core.getBooleanInput('clear');
 
   const pullRequest = (github.context.payload as PullRequestWebhookPayload).pull_request;
   if (!pullRequest) {
-    const message = 'Could not get pull_request from context, exiting';
-    console.log(message);
-    if (!silent) {
-      throw Error(message);
-    }
+    error('Could not get pull_request from context, exiting');
     return;
   }
 
   const {
-    milestone,
+    milestone: currMilestone,
     number: prNumber,
     base: { ref: baseBranch },
     head: { ref: headBranch },
   } = pullRequest;
 
-  if (milestone) {
-    console.log('Milestone already exists, exiting');
-    return;
+  core.setOutput('previous', currMilestone?.title);
+  core.setOutput('milestone', currMilestone?.title);
+
+  if (currMilestone) {
+    console.log(`Milestone already exists: ${currMilestone.title}`);
+    if (!force && !clear) {
+      console.log('Exiting');
+      return;
+    }
   }
 
   const client = new github.GitHub(token);
-
   const configObject = await config(client, configPath);
   console.log('configObject base-branch', configObject.baseBranchList);
   console.log('configObject head-branch', configObject.headBranchList);
 
   const addMilestone = match(baseBranch, headBranch, configObject);
 
-  if (!addMilestone) {
-    console.log('Milestone not hit, exiting');
-    return;
-  }
+  if (addMilestone) {
+    console.log(`Milestone hit: ${addMilestone}`);
+    if (currMilestone?.title === addMilestone) {
+      console.log(`Milestone already set to ${addMilestone}, exiting.`);
+      return;
+    }
 
-  const milestones = await client.issues.listMilestonesForRepo({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-  });
-  const { number: milestoneNumber } = milestones.data.find(({ title }) => title === addMilestone)
-    || {};
-
-  if (milestoneNumber) {
+    if (!currMilestone || force) {
+      console.log(`Update of milestone from ${currMilestone?.title} to ${addMilestone}`);
+      const milestones = await client.issues.listMilestonesForRepo({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+      });
+      const newMilestone = getMilestoneNumber(milestones, addMilestone);
+      if (!newMilestone) {
+        error(`Milestone "${addMilestone}" not found, Please create it first.`);
+        return;
+      }
+      await client.issues.update({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        issue_number: prNumber,
+        milestone: newMilestone,
+      });
+      core.setOutput('milestone', addMilestone);
+    } else {
+      console.log(`Do not correct milestone from ${currMilestone.title} to ${addMilestone}`);
+    }
+  } else if (clear && currMilestone) {
+    console.log(`Clear milestone ${currMilestone.title} due to clear flag`);
     await client.issues.update({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       issue_number: prNumber,
-      milestone: milestoneNumber,
+      milestone: null,
     });
-    core.setOutput('milestone', addMilestone);
+    core.setOutput('milestone', null);
   } else {
-    const message = `Milestone not found, Please create it first "${addMilestone}".`;
-    console.log(message);
-    if (!silent) {
-      throw Error(message);
-    }
+    console.log(`Milestone not changed: ${currMilestone?.title}`);
   }
 }
 
-run().catch((error) => {
-  core.setFailed(error.message);
+run().catch((err) => {
+  core.setFailed(err.message);
 });
